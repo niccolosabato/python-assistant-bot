4628b9233188592f53f7722676d54430691a3262
sabato <niccolo.s04@gmail.com>
2022-06-24 23:02:14 +0200

    print on request

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 print("Welcome to fAssistant")
 print("I can just print the time and the date for now...")
+print('Type "time" to print the time or "date" to print the date:')
+
+request=input()
 import time
-print(time.strftime("%H.%M.%S"))
-print(time.strftime("%d.%m.%Y"))
+
+if input=="date":
+    print(time.strftime("%H.%M.%S"))
+else:
+    print(time.strftime("%d.%m.%Y"))
+
